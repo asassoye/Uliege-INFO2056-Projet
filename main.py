0e3624a6e62396349e10820cd60114852da1f1a9
Andrew Sassoye <andrew.sassoye@student.ulg.ac.be>
2016-12-04 14:59:59 +0100

    Création de la classe World

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import pygame
-
 pygame.init()
+
+GRANDEUR = 64
+
+
+class World:
+    def __init__(self):
+        self.surface = pygame.display.set_mode([30 * GRANDEUR, 15 * GRANDEUR])
+        pygame.display.flip()
+
