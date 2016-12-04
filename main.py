1a5fbf82a2c15dde1a44cb1fd4c3bcfcc0bc6534
Andrew Sassoye <andrew.sassoye@student.ulg.ac.be>
2016-12-04 15:13:17 +0100

    Création de la classe Carte

--- main.py
+++ main.py
@@ -9,3 +9,22 @@ class World:
         self.surface = pygame.display.set_mode([30 * GRANDEUR, 15 * GRANDEUR])
         pygame.display.flip()
 
+
+class Carte:
+    def __init__(self, elements, portes):
+        self.elements = elements
+        self.portes = portes
+
+    def afficheCarte(self):
+        i=0
+
+        for lines in self.elements:
+            j=0
+
+            for element in lines:
+                imageBlock[element].dessine([j, i])
+                j += 1
+
+            i += 1
+
+        pygame.display.flip()
